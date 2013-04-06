bd3736154b83e1e6265984306bdf52496ed30bc8
Bennett Wilson <bennettwilson@gmail.com>
2013-04-06 12:51:22 -0400

    initial commit

---
+++ app/assets/javascripts/backbone/app.js
@@ -0,0 +1,11 @@
+var Track = function(Backbone, Marionette) {
+
+
+  var App = new Marionette.Application();
+
+  App.addRegions({
+
+  });
+};
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Track2::Application.config.secret_token = '090b9220f3dd5cd6b5378fe1be21e5bfde97cd25522415a94df62b7f0e27ddc55d24fb9d5b2ad30b2457ffe2f9b810fa29631d663303c268616b1a957ed0b7eb'
